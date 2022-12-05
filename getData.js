11941b606740185a7897c0df9f320de74150d4b5
abloodgood <114004494+abloodgood@users.noreply.github.com>
2022-12-04 19:16:39 -0500

    lots of updates

---
+++ getData.js
@@ -0,0 +1,28 @@
+const mainContainer = document.getElementById("jsonData");
+
+const fetchData = async () => {
+		for (let i=1; 1 <= 3; i++) {
+				await getDegree(i);
+			}
+};
+
+const getDegree = async () => {
+			const url = "https://abloodgood.github.io/assignment5/my_degrees.json"
+			const result = await fetch(url);
+			const degree = await res.json();
+			console.log(degree);
+			displayDegree(degree);
+};
+
+fetchData();
+
+function displayDegree(degree) {
+			const degreeElement = document.createElement("div")
+			degreeElement.classList.add("degree");
+			const school = degree.school;
+			const major = degree.major;
+			const type = degree.type;
+			const year = degree.year;
+			mainContainer.appendChild(degreeElement);
+}
+
